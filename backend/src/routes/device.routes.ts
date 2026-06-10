4821b413760aa3d7f4b94786d98f437dd5c99771
yooerizkilab <yooerizkilab@webmail.umm.ac.id>
2026-06-10 22:07:33 +0700

    feat: enforce subscription check on sensitive API routes

--- backend/src/routes/device.routes.ts
+++ backend/src/routes/device.routes.ts
@@ -1,13 +1,13 @@
 import { FastifyInstance } from 'fastify';
 import { deviceController } from '../controllers/deviceController';
-import { authenticate } from '../middlewares/auth';
+import { authenticate, hasActiveSubscription } from '../middlewares/auth';
 import { quotaMiddleware } from '../middlewares/quotaMiddleware';
 
 export async function deviceRoutes(fastify: FastifyInstance) {
     fastify.addHook('preHandler', authenticate);
 
     fastify.get('/', deviceController.list);
-    fastify.post('/connect', { preHandler: [quotaMiddleware.checkDeviceQuota] }, deviceController.connect);
+    fastify.post('/connect', { preHandler: [hasActiveSubscription, quotaMiddleware.checkDeviceQuota] }, deviceController.connect);
     fastify.get('/:id/status', deviceController.getStatus);
     fastify.delete('/:id', deviceController.disconnect);
 }
--- backend/src/routes/message.routes.ts
+++ backend/src/routes/message.routes.ts
@@ -1,16 +1,16 @@
 import { FastifyInstance } from 'fastify';
 import { messageController } from '../controllers/messageController';
 import { blastController } from '../controllers/blastController';
-import { authenticate } from '../middlewares/auth';
+import { authenticate, hasActiveSubscription } from '../middlewares/auth';
 import { quotaMiddleware } from '../middlewares/quotaMiddleware';
 
 export async function messageRoutes(fastify: FastifyInstance) {
     fastify.addHook('preHandler', authenticate);
 
-    fastify.post('/send', { preHandler: [quotaMiddleware.checkMessageQuota] }, messageController.send);
+    fastify.post('/send', { preHandler: [hasActiveSubscription, quotaMiddleware.checkMessageQuota] }, messageController.send);
     fastify.get('/logs', messageController.getLogs);
     fastify.get('/logs/report', messageController.downloadReport);
-    fastify.post('/blast', blastController.create);
+    fastify.post('/blast', { preHandler: [hasActiveSubscription] }, blastController.create);
     fastify.get('/blast', blastController.list);
     fastify.get('/blast/:id', blastController.getJob);
     fastify.get('/blast/:id/report', blastController.downloadReport);
